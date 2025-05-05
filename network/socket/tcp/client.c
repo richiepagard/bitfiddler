c9f7d039ae1d805d87e6f299ad204444557a0f70
richie <richiepagard@gmail.com>
2025-05-05 19:52:30 +0330

    feat: Implement the client side of the socket programming.
    - Implement the client side of the socket programming according to the `network/socket/tcp/README`.

---
+++ network/socket/tcp/client.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+
+
+void client(char *message, char *server_ip, int server_port);
+
+
+int main() {
+    client("Hello World!", "127.0.0.1", 8080);
+
+    return 0;
+}
+
+
+void client(char *message, char *server_ip, int server_port) {
+    /*
+    The client side used to send requests to the server.
+
+    Arguments:
+        message(string): The data will be sent to the server.
+        server_ip(string): The IP of the server(e.g., 127.0.0.1) to connect to it.
+        server_port(int): The port number of the server to connect to.
+
+    NOTE:
+        htons: make sure the port number is stored in the correct byte order for network communication.
+        inet_pton: converts a human-readable IP address(127.0.0.1) to a binary format that sockets need.
+            So, string IP becomes packet network format.
+    */
+
+    int sockfd = 0;
+    char buffer[1024] = {0};
+
+    // A structure to hold the server info
+    struct sockaddr_in server_addr;
+
+    // Create a socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // Check the socket creation status(failed or successful)
+    if(sockfd < 0) {
+        perror("Socket Creation Failed.\n");
+        exit(1);
+    }
+
+    // Define server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+
+    // Convert IP to binary and set it
+    if(inet_pton(AF_INET, server_ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid Server Address.\n");
+        exit(1);
+    }
+
+    // Connect to server
+    if(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection To The Server Failed.\n");
+        exit(1);
+    }
+
+    // Send message
+    ssize_t bytes_sent = send(sockfd, message, strlen(message), 0);
+    if(bytes_sent < 0) {
+        perror("Failed To Send Data To The Server.\n");
+        exit(1);
+    }
+
+    // Receive response from the server and print the buffer data
+    ssize_t bytes_read = read(sockfd, buffer, sizeof(buffer)-1);
+    if(bytes_read < 0) {
+        perror("Receiving Data From The Server Failed\n");
+        exit(1);
+    }
+
+    // Ensure null-terminated
+    buffer[bytes_read] = '\0';
+
+    printf("Server Respond: %s\n", buffer);
+
+    // Close the connection
+    close(sockfd);
+}
