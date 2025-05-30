44e970166f7267d508dca6577a21fc346898eb2e
richie <richiepagard@gmail.com>
2025-05-31 00:13:34 +0330

    feat: Solve/Finish/Study the histogram character exercise from the `K & R` book.

---
+++ c-exercise/kr-book/histogram-characters/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#define ARRAY_SIZE 26
+
+void histogramCharacters(void);
+
+int main() {
+    histogramCharacters();
+
+    return 0;
+}
+
+
+void histogramCharacters(void) {
+    int character;
+    int letters[ARRAY_SIZE];
+
+    // Fill the `letters` array
+    for (int item = 0; item < ARRAY_SIZE; ++item) letters[item] = 0;
+
+    while ((character = getchar()) != EOF)
+    {
+        // Check if the current character is alpha,
+        // increment the place of the current character in the array (alphabet).
+        if (character >= 'a' && character <= 'z') ++letters[character - 'a'];
+        if (character >= 'A' && character <= 'Z') ++letters[character - 'A'];
+    }
+
+    for (int item = 0; item < ARRAY_SIZE; ++item)
+    {
+        // Only letters that appeared in the input
+        if (letters[item] > 0) {
+            // Convert the current letter to its place in alphabet
+            if (letters[item] >= 'a' || letters[item] <= 'z') printf("[%c], ", item + 'a');
+            if (letters[item] >= 'A' || letters[item] <= 'Z') printf("[%c]: ", item + 'A');
+
+            // Print how many times the current letter appeared
+            printf("%d", letters[item]);
+            printf("\n");
+        }
+    }
+}
