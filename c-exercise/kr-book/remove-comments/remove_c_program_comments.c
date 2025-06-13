7f53b3a96a736b8a8038cd4832727dfb2c412d49
richie <richiepagard@gmail.com>
2025-06-13 20:41:36 +0330

    feat: Improve `commentRemover` function by ignore the single-line comments properly. `c-exercise/kr-book/remove-comments/remove_c_program_comments.c`.

--- c-exercise/kr-book/remove-comments/remove_c_program_comments.c
+++ c-exercise/kr-book/remove-comments/remove_c_program_comments.c
@@ -21,11 +21,45 @@ void commentRemover(int buffer_size) {
 	int character;
 	int counter;
 	char buffer[buffer_size];
+	char output[buffer_size];
 
+	// Fill the buffer character by character with input
 	for (counter = 0; (counter < buffer_size-1) && ((character = getchar()) != EOF); ++counter) buffer[counter] = character;
 
-	buffer[counter] = '\0';
-	++counter;
+	// If newline is encountered, store it in the buffer
+	if (character == '\n')
+	{
+		buffer[counter] = character;
+		++counter;
+	}
 
-	printf("%s\n", buffer);
+	// Set null-terminate to the string
+	buffer[counter] = '\0';
+
+	int i = 0, j = 0;
+	while (buffer[i] != '\0')
+	{
+		if (buffer[i] == '/' && buffer[i + 1] == '/')
+		{
+			// Skip the rest of the current line to ignore the single-line comments
+			while (buffer[i] != '\n' && buffer[i] != '\0') ++i;
+			// Skip the newline/tab after comment to avoid affecting the next line
+			continue;
+		}
+		else
+		{
+			// Copy the character to the output buffer 
+			output[j] = buffer[i];
+			++j;
+		}
+
+		if (buffer[i] != '\0') ++i;
+	}
+
+	output[j] = '\0';
+
+	printf("------------------\n");
+	printf("The Cleaner Version Of The Input: \n\n```c\n");
+	printf("%s", output);
+	printf("```\n");
 }
