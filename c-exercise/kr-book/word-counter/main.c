e63e0ef32db5203b015d27f43171cf834110b36c
Richie <richiepagard@gmail.com>
2025-11-09 11:38:21 -0500

    doc: Write doc-string for `counter` method of the `word-counter` module.

--- c-exercise/kr-book/word-counter/main.c
+++ c-exercise/kr-book/word-counter/main.c
@@ -11,6 +11,10 @@ int main() {
 
 
 void counter(void) {
+	/* Word counter function to count words which
+	 * contain ASCII letters, it does not count words
+	 * which only contain none ASCII letters.
+	 */
 	int character;
 	int words_num;
 
